460fdde7d0ff47f4ce9fbd10481d44f0e1b0c969
Muiz Musaddik <muizmusaddik@gmail.com>
2015-03-02 15:10:47 +0800

    Question 4

---
+++ golang/0004_largest_palindrome_product.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"strconv"
+)
+
+func isPalindromeNumber(number int) bool {
+	arrays := strings.Split(strconv.Itoa(number), "")
+	length := math.Floor(float64(len(arrays) / 2))
+	for i := 0; i < int(length); i++ {
+		if arrays[i] != arrays[len(arrays) - 1 - i] {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	var found bool = false
+	for i := 998001; i > 0; i-- {
+		if isPalindromeNumber(i) {
+			for j := 999; j > 899; j-- {
+				if i % j == 0 {
+					tmp := i / j
+					for k := 999; k > 899; k-- {
+						if tmp % k == 0 {
+							fmt.Println("Largest palindrome number made from the product of 2 3-digit number is: ", i)
+							found = true
+							break
+						}
+					}
+					//found = true
+					//break
+				}
+				if found {
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+	}
+}
+
