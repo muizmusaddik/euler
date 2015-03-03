c0ae1f8b557b5b56ea37579e0d861d7076e80db0
Muiz Musaddik <muizmusaddik@gmail.com>
2015-03-03 14:32:43 +0800

    Question 9

--- golang/0009_special_pythagorean_triplet.go
+++ golang/0009_special_pythagorean_triplet.go
@@ -5,21 +5,32 @@ import (
 	"math"
 )
 
-func greatestCommonDeviser(i float64, j, float64, k float64) float64 {
+func greatestCommonDevisor(a int, b int, c int) int {
+	var tmp_1 int 
+	var tmp_2 int
 
+	for c % a != 0 && b % a != 0 {
+		tmp_1 = c
+		tmp_2 = b
+		c = a
+		b = tmp_1 % a
+		a = tmp_2 % a
+	}
+
+	return a
 }
 
-func isCoprime(i float64, j float64) bool {
+func isCoprime(i int, j int) bool {
 	var biggest int
 
 	if i > j {
-		biggest = int(i)
+		biggest = i
 	} else {
-		biggest = int(j)
+		biggest = j
 	}
 
-	for k := 2; k <= int(biggest); k++ {
-		if int(i)%k == 0 && int(j)%k == 0 {
+	for k := 2; k <= biggest; k++ {
+		if i%k == 0 && j%k == 0 {
 			return false
 		}
 	}
@@ -27,12 +38,18 @@ func isCoprime(i float64, j float64) bool {
 }
 
 func main() {
-	for m := float64(2); m <= float64(100); m++ {
-		for n := float64(1); n < m; n++ {
-			if isCoprime(n, m) {
-				a, b, c := math.Pow(m, 2)-math.Pow(n, 2), 2*m*n, math.Pow(m, 2)+math.Pow(n, 2)
-				fmt.Println(a, b, c, ":", a+b+c)
+	// Euclid's formula
+	// a + b + c = m^2 - n^2 + 2mn + (m^2 + n^2) = 1000
+	// 2 * m * (m + n) = 1000
+
+	var total int = 1000
+	for m := 2; m <= total; m++ {
+		for n := 1; n <= m; n++ {
+			if 2 * m * (m + n) == total {
+				a, b, c := math.Pow(float64(m), 2)- math.Pow(float64(n), 2), 2 * float64(m) * float64(n), math.Pow(float64(m), 2) + math.Pow(float64(n), 2)
+				fmt.Println(a, b, c, int(a * b * c))
 			}
 		}
 	}
 }
+
